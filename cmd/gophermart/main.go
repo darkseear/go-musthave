2c3f59f882fc7a3272d38e513289594f6a6fae80
DarkseearIS <darkseeara@yandex.ru>
2025-03-13 18:00:44 +0300

    init db config

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -4,8 +4,11 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"go.uber.org/zap"
 
 	"github.com/darkseear/go-musthave/internal/config"
+	"github.com/darkseear/go-musthave/internal/database"
 	logger "github.com/darkseear/go-musthave/internal/logging"
 )
 
@@ -22,5 +25,28 @@ func run() error {
 	if err := logger.Initialize(LogLevel); err != nil {
 		return err
 	}
-	return http.ListenAndServe(config.Address, chi.NewRouter())
+
+	//инициализировать дб
+	db, err := database.InitDB(config.Database)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	//миграции
+	err = database.RunMigrations(db)
+	if err != nil {
+		panic(err)
+	}
+
+	//создать роутер
+	r := chi.NewRouter()
+	r.Use(middleware.Recoverer)
+
+	// r.Group(func(r chi.Router){
+	// 	r.Post("/api/users/register", )//решистрация пользователя
+	// })
+
+	logger.Log.Info("Running server", zap.String("address", config.Address))
+	return http.ListenAndServe(config.Address, r)
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -6,26 +6,28 @@ import (
 )
 
 type Config struct {
-	Address   string
-	URL       string
-	LogLevel  string
-	Database  string
-	SecretKey string
+	Address              string
+	URL                  string
+	LogLevel             string
+	Database             string
+	SecretKey            string
+	AccrualSystemAddress string
 }
 
 func New() *Config {
 	var config Config
 
-	flag.StringVar(&config.Address, "a", "localhost:8080", "server url")
-	flag.StringVar(&config.URL, "b", "http://localhost:8080", "last url")
+	flag.StringVar(&config.Address, "a", "localhost:8081", "server url")
+	flag.StringVar(&config.URL, "b", "http://localhost:8081", "last url")
 	flag.StringVar(&config.LogLevel, "l", "info", "log level")
-	// flag.StringVar(&config.Database, "d", "host=localhost user=postgres password=1234567890 dbname=shorten sslmode=disable", "Database")
-	flag.StringVar(&config.Database, "d", "", "Database")
+	flag.StringVar(&config.Database, "d", "host=localhost user=postgres password=1234567890 dbname=loyalty sslmode=disable", "Database")
+	// flag.StringVar(&config.Database, "d", "", "Database")
 	flag.StringVar(&config.SecretKey, "s", "secretkey", "Key for JWT")
+	flag.StringVar(&config.AccrualSystemAddress, "r", "localhost:8080", "Accrual System Address")
 
 	flag.Parse()
 
-	if val, state := os.LookupEnv("SERVER_ADDRESS"); state {
+	if val, state := os.LookupEnv("RUN_ADDRESS"); state {
 		config.Address = val
 	}
 	if val, state := os.LookupEnv("BASE_URL"); state {
@@ -34,7 +36,7 @@ func New() *Config {
 	if val, state := os.LookupEnv("LOG_LEVEL"); state {
 		config.LogLevel = val
 	}
-	if val, state := os.LookupEnv("DATABASE"); state {
+	if val, state := os.LookupEnv("DATABASE_URI"); state {
 		config.Database = val
 	}
 	if val, state := os.LookupEnv("SECRET_KEY"); state {
