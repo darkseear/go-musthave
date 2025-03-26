6603a00d85235f46e09ebb5168c45220040501f8
DarkseearIS <darkseeara@yandex.ru>
2025-03-26 17:46:44 +0300

    auth

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,21 +1,24 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	// "github.com/go-chi/chi/v5/middleware"
 	"go.uber.org/zap"
 
 	"github.com/darkseear/go-musthave/internal/config"
 	"github.com/darkseear/go-musthave/internal/database"
 	logger "github.com/darkseear/go-musthave/internal/logging"
+	"github.com/darkseear/go-musthave/internal/middleware"
+	"github.com/darkseear/go-musthave/internal/service"
 )
 
 func main() {
 	if err := run(); err != nil {
 		logger.Log.Error("Start server anormal")
-		panic(err)
+		log.Fatal(err)
 	}
 }
 
@@ -29,24 +32,27 @@ func run() error {
 	//инициализировать дб
 	db, err := database.InitDB(config.Database)
 	if err != nil {
-		panic(err)
+		logger.Log.Error("Failed to initialize database")
+		log.Fatal(err)
 	}
 	defer db.Close()
 
 	//миграции
 	err = database.RunMigrations(db)
 	if err != nil {
-		panic(err)
+		logger.Log.Error("Failed to run migrations")
+		log.Fatal(err)
 	}
 
+	auth := service.NewAuth(config.SecretKey)
 	//создать роутер
 	r := chi.NewRouter()
-	r.Use(middleware.Recoverer)
+	// r.Use(middleware.Recoverer)
 
 	// r.Group(func(r chi.Router){
 	// 	r.Post("/api/users/register", )//решистрация пользователя
 	// })
 
 	logger.Log.Info("Running server", zap.String("address", config.Address))
-	return http.ListenAndServe(config.Address, r)
+	return http.ListenAndServe(config.Address, middleware.AuthMiddleware(r, auth))
 }
--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,1 +1,26 @@
 package middleware
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/darkseear/go-musthave/internal/service"
+)
+
+func AuthMiddleware(next http.Handler, a *service.Auth) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		if token == "" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		userID, err := a.ValidateToken(token)
+		if err != nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		ctx := context.WithValue(r.Context(), "userID", userID)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r)
+	})
+}
